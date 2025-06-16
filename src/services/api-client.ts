b00a47b8faf3fdac79b5cd7d346329d05d961d12
3liyounis <aliyounis642@gmail.com>
2025-06-16 21:27:43 +0300

    Stop Tracking api-client.ts

--- src/services/api-client.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { AxiosError, CanceledError } from "axios";
-export default axios.create({
-    baseURL: "https://api.rawg.io/api",
-    params: {
-        key: "THIS-IS-A-SECRET"
-    }
-})
-export { AxiosError, CanceledError };
